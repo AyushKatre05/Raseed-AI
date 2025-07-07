2000789282dab4784ecd5c60b1754e3729269f36
Ayush Katre <ayushkatre1801@gmail.com>
2025-07-07 12:06:23 +0530

    first commit

---
+++ server/src/routes/authRoutes.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+const router = express.Router();
+const { register, login } = require('../controllers/authController');
+
+// Public routes
+router.post('/signup', register);
+router.post('/login', login);
+
+export default router;
